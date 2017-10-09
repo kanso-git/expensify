246952a0102da5403c61bb710fd442530b0fc575
Abdallah Kanso <abdallah.kanso@gmail.com>
2017-10-09 20:56:30 +0200

    add date and amount formatting

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,14 +1,8 @@
-// react-test-renderer
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 import Header from '../../components/Header';
-import toJson from 'enzyme-to-json';
 
-
-
-test('Should render Header component correctly',()=>{
-    const wrapper = shallow(<Header/>);
-
-    expect(wrapper).toMatchSnapshot();
-
+test('should render Header correctly', () => {
+  const wrapper = shallow(<Header />);
+  expect(wrapper).toMatchSnapshot();
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,38 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
+module.exports = (env, argv)=>{
+
+  return {
+      entry: './src/app.js',
+      output: {
+        path: path.join(__dirname, 'public','dist'),
+        filename: 'bundle.js'
+      },
+      module: {
+        rules: [{
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        }, {
+          test: /\.s?css$/,
+          use:ExtractTextPlugin.extract([ 'css-loader','sass-loader' ])
+        }]
+      },
+      plugins: [
+          new ExtractTextPlugin({
+            filename: '[name].css'
+          })
+        ]
+      
+,
+      devtool: env ==='prod'?'source-map':'cheap-module-eval-source-map',
+      devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true,
+        publicPath: "/dist/"
+        
+      }
+    }
   }
-};
+
